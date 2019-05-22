11690b95518bdc4803c902896a85fb7d7a7e408b
Jesús Miguel Cruz Cana <jesus.cruz@mimacom.com>
2019-05-22 10:12:59 +0200

    Removed commented line.

--- vuejs/src/domain/common.ts
+++ vuejs/src/domain/common.ts
@@ -3,7 +3,6 @@ import { throwError } from 'rxjs';
 import store from '../stores/central.store';
 
 export const showSnackbarAndReturnError = (error: Error) => {
-  // alert(error);
   store.dispatch('setSnackbar', {
     text: error.message,
     color: 'red',
